97da7a5bcdd983dc1fb231fb70e23916f6c01c42
blkluv <info@blkluv.org>
2023-07-05 14:19:04 -0400

    updated index

--- frontend/components/common/Footer.tsx
+++ frontend/components/common/Footer.tsx
@@ -5,24 +5,31 @@ function Footer() {
     <div
       className="py-3 d-flex justify-content-center"
       style={{
-        backgroundColor: "rgba(0, 0, 0, 0.70)", //"rgb(232, 232, 232)"
+        backgroundColor: "#222",
         color: "white",
+        fontSize: "14px",
       }}
     >
-      <p>
-        {" "}
+      <p style={{ margin: 0 }}>
         Leave a ⭐ on{" "}
         <a
           href="https://github.com/J0SAL/Decentralized-Expense-Tracker"
           target="_blank"
           rel="noreferrer"
+          style={{ color: "white", textDecoration: "none" }}
         >
           Github
         </a>
-        <span className="px-2">, made by</span>
-        <a href="https://bio.link/j0sal" target="_blank" rel="noreferrer">
-          @Joy Almeida
-        </a>{" "}
+        <span style={{ padding: "0 8px" }}>|</span>
+        Made by{" "}
+        <a
+          href="https://bio.link/j0sal"
+          target="_blank"
+          rel="noreferrer"
+          style={{ color: "white", textDecoration: "none" }}
+        >
+          @ARVRtise
+        </a>
       </p>
     </div>
   );
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 import Overview from "../components/overview/Overview";
 import PerformanceGraph from "../components/performance/PerformanceGraph";
 import Performers from "../components/top-performers/Performers";
@@ -16,7 +16,7 @@ export default function HomePage() {
   const handleScroll = () => {
     if (window.scrollY > 300) {
       setShowButton(true);
-    } else if(window.scrollY===0){
+    } else if (window.scrollY === 0) {
       setShowButton(false);
     }
   };
@@ -24,7 +24,7 @@ export default function HomePage() {
   useEffect(() => {
     window.addEventListener("scroll", handleScroll, true);
     return () => window.removeEventListener("scroll", handleScroll, true);
-  });
+  }, []); // Add an empty dependency array to run the effect only once on component mount
 
   const scrollToTop = () => {
     window.scrollTo(0, 0);
@@ -39,6 +39,15 @@ export default function HomePage() {
   if (!isWeb3Enabled) return <Login />;
   return (
     <div style={{ backgroundColor: "rgba(232, 249, 252, 0.76)" }}>
+      <div className="hero-section">
+        <div className="overlay">
+          <h1>Welcome to Your App</h1>
+          <p>Connect with MetaMask to get started</p>
+          <Button variant="primary">Connect to MetaMask</Button>
+        </div>
+        <img src="hero-image.jpg" alt="Hero Image" />
+      </div>
+
       <Container className="mt-5 py-3 bg-white">
         <Overview />
         {transactions.length > 0 && (
@@ -48,7 +57,56 @@ export default function HomePage() {
             <Performers />
           </div>
         )}
+
+        <div className="section">
+          <Row>
+            <Col md={6}>
+              <img src="section1-image.jpg" alt="Section 1" />
+            </Col>
+            <Col md={6}>
+              <h2>Section 1 Title</h2>
+              <p>Section 1 Description</p>
+            </Col>
+          </Row>
+        </div>
+
+        <div className="section">
+          <Row>
+            <Col md={6}>
+              <h2>Section 2 Title</h2>
+              <p>Section 2 Description</p>
+            </Col>
+            <Col md={6}>
+              <img src="section2-image.jpg" alt="Section 2" />
+            </Col>
+          </Row>
+        </div>
+
+        <div className="section">
+          <Row>
+            <Col md={6}>
+              <img src="section3-image.jpg" alt="Section 3" />
+            </Col>
+            <Col md={6}>
+              <h2>Section 3 Title</h2>
+              <p>Section 3 Description</p>
+            </Col>
+          </Row>
+        </div>
+
+        <div className="section">
+          <Row>
+            <Col md={6}>
+              <h2>Section 4 Title</h2>
+              <p>Section 4 Description</p>
+            </Col>
+            <Col md={6}>
+              <img src="section4-image.jpg" alt="Section 4" />
+            </Col>
+          </Row>
+        </div>
       </Container>
+
       <button
         className="btn"
         style={{
