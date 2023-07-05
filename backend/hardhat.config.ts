7c7a6e2b94f0bec30b22127535bbee45d7ea1974
blkluv <info@blkluv.org>
2023-07-05 14:01:17 -0400

    updated backend

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,48 +1,46 @@
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "solidity-coverage";
-import "dotenv/config";
-import "./tasks/accounts";
-import "@typechain/hardhat";
-import "hardhat-deploy";
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("solidity-coverage");
+require("dotenv/config");
+require("dotenv").config();
+require("./tasks/accounts");
+require("@typechain/hardhat");
+require("hardhat-deploy");
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https://eth-goerli.g.alchemy.com/v2/JoNBVOasNy_Ne7dP22jHEuamkurKBCnh";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xAD9d8988E2d36a9fC693b4Ea0fd5c55759fDE4b1";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "X4UW91KJ192AQXFTTW6K71SJH7YV3DPVS6";
 
 module.exports = {
   defaultNetwork: "hardhat",
-    networks: {
-      hardhat: {
-        chainId: 31337
-      },
-      localhost: {
-          chainId: 31337,
-      },
-      goerli: {
-          url: GOERLI_RPC_URL,
-          accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-          saveDeployments: true,
-          chainId: 5,
-      },
+  networks: {
+    hardhat: {
+      chainId: 31337,
     },
-
-    solidity: {
-      compilers: [
-        {
-          version: "0.8.7",
-        },
-      ],
+    localhost: {
+      chainId: 31337,
     },
-
-    etherscan: {
-      apiKey: ETHERSCAN_API_KEY,
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      saveDeployments: true,
+      chainId: 5,
     },
-
-    namedAccounts: {
-      deployer: {
-          default: 0, 
-      }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+    ],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
   },
 };
